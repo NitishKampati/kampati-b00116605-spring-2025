99a2ba8abdc4bc00030aff101da07b4206d43235
NitishKampati <163798491+NitishKampati@users.noreply.github.com>
2025-02-11 02:05:55 -0600

    Update program.py

---
+++ src/program.py
@@ -0,0 +1,15 @@
+import numpy as 
+import pandas as pd
+import os
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D,Flatten,Dense,MaxPooling2D,Dropout
+from sklearn.metrics import accuracy_score
+import ipywidgets as widgets
+import io
+from PIL import Image
+import tqdm
+from sklearn.model_selection import train_test_split
+import cv2
+from sklearn.utils import shuffle
+import tensorflow as tf
