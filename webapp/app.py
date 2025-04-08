7532ebdc76bc0ac49624ed51ffbd8d47fd983306
NitishKampati <nitishkampati@gmail.com>
2025-04-08 11:44:03 -0500

    Added upload route

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="images")
+
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
